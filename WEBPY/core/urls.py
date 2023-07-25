8e6335946ef0fe9fc66d29a3754338f6610b7117
bragonzu <bragonzu@hotmail.com>
2023-07-24 21:04:46 -0500

    [IMP] sistema de inventarios

--- WEBPY/core/urls.py
+++ WEBPY/core/urls.py
@@ -1,7 +1,7 @@
 """core URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/4.1/topics/http/urls/
+    https://docs.djangoproject.com/en/3.1/topics/http/urls/
 Examples:
 Function views
     1. Add an import:  from my_app import views
@@ -15,17 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path,include
-from django.conf import settings
-from django.conf.urls.static import static
-
-from myapp import views as user_views
-from django.contrib.auth import views as authentication_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('myapp.urls')),
-    path('register/', user_views.register,name='register'),
-    path('login/', authentication_views.LoginView.as_view(template_name='users/login.html'),name='login'),
-    path('logout/', authentication_views.LogoutView.as_view(template_name='users/logout.html'),name='logout'),
+    path('',include('myapp.urls'))
+
 ]
-urlpatterns += static(settings.STATIC_URL , document_root = settings.STATIC_ROOT)
